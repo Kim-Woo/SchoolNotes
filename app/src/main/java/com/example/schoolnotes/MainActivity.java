81168e5081719ffcb0d1bbdb2fba9f587041e1b3
Kim-Woo <80669680+Kim-Woo@users.noreply.github.com>
2021-12-06 22:25:20 +0800

    create schedule

--- app/src/main/java/com/example/schoolnotes/MainActivity.java
+++ app/src/main/java/com/example/schoolnotes/MainActivity.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     final private int [] ButtonRes = new int [] {R.id.s390f, R.id.a309bf, R.id.s496f, R.id.s413f, R.id.a310bf };
     CoursesList coursesList;
     Course[] courses;
+    View [] upComing;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +37,6 @@ public class MainActivity extends AppCompatActivity {
         createSchedule();
     }
 
-    private void createSchedule() {
-
-    }
 
 
     @Override
@@ -46,12 +49,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.about:
-                // User chose the "Settings" item, show the app settings UI...
+                //User chose the "About" item, show the about page...
+                Intent intent = new Intent(MainActivity.this,About.class);
+                startActivity(intent);
                 return true;
 
             case R.id.setting:
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
+                // User chose the "Settings" item, show the app settings UI...
                 return true;
 
             default:
@@ -80,5 +84,37 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void createSchedule() {
+        upComing = new View[3];
+        upComing[0] = findViewById(R.id.upcoming_1);
+        upComing[1] = findViewById(R.id.upcoming_2);
+        upComing[2] = findViewById(R.id.upcoming_3);
+
+        TextView [] courseCode = new TextView[3];
+        TextView [] courseTime = new TextView[3];
+        TextView [] lecturer = new TextView[3];
+        TextView [] location = new TextView[3];
+
+        Course[] theCourses = new Course[3];
+        theCourses[0] = courses[3];
+        theCourses[1] = courses[1];
+        theCourses[2] = courses[1];
+        for (int i=0; i<upComing.length;i++){
+            courseCode[i] = upComing[i].findViewById(R.id.uc_courseCode);
+            courseTime[i] = upComing[i].findViewById(R.id.uc_courseTime);
+            lecturer[i] = upComing[i].findViewById(R.id.uc_courseLecturerName);
+            location[i] = upComing[i].findViewById(R.id.uc_courseLocation);
+
+            courseCode[i].setText(theCourses[i].getCourseCode());
+            courseTime[i].setText(theCourses[i].getTutorialWeekDay()+" "+theCourses[i].getTutorialTime());
+            lecturer[i].setText(theCourses[i].getCourseLecturerName());
+            location[i].setText(theCourses[i].getTutorialLocation());
+
+            if (i==1){
+                courseTime[i].setText(theCourses[i].getLectureWeekDay()+" "+theCourses[i].getLectureTime());
+            }
+        }
+
+    }
 
 }
